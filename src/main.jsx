db092cd91ce5af7c8e22926bfd6f9ea3de9edf4e
rytesdd <gugewang530@gmail.com>
2026-01-30 17:36:21 +0800

    自动保存: 17:36:21

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ConfigProvider } from 'antd'
+import zhCN from 'antd/locale/zh_CN'
+import { XProvider } from '@ant-design/x'
 import App from './App.jsx'
 import './index.css'
 
+const theme = {
+  token: {
+    fontSize: 12,
+    borderRadius: 6,
+  },
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider locale={zhCN} theme={theme}>
+      <XProvider>
+        <App />
+      </XProvider>
+    </ConfigProvider>
   </React.StrictMode>,
 )
