fe4fe9798a9ccb108ac4a9c6d8b65fc8131a6b8d
rytesdd <gugewang530@gmail.com>
2026-01-30 17:36:29 +0800

    自动保存: 17:36:29

--- src/chat-main.jsx
+++ src/chat-main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ConfigProvider } from 'antd'
+import zhCN from 'antd/locale/zh_CN'
+import { XProvider } from '@ant-design/x'
 import AiChatDashboard from './AiChatDashboard.jsx'
 import './index.css'
 
+const theme = {
+  token: {
+    fontSize: 12,
+    borderRadius: 6,
+  },
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AiChatDashboard />
+    <ConfigProvider locale={zhCN} theme={theme}>
+      <XProvider>
+        <AiChatDashboard />
+      </XProvider>
+    </ConfigProvider>
   </React.StrictMode>,
 )
